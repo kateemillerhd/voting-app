e2119f71db798c60f8ca79ebba4bf3ea8fc7eea6
kateemillerhd <kateemillerhd@gmail.com>
2025-07-01 08:37:58 +0000

    Create User model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,11 @@
-// MongoDB model
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  provider: String,
+  providerId: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("User", userSchema);
