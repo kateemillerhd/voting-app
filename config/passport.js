480a4d8aa80c41ab1735f74b8e31b9e4c847b3ce
kateemillerhd <kateemillerhd@gmail.com>
2025-07-01 09:07:13 +0000

    Created index.html and updated a couple other files

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User');
+
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
+        if (!user) return done(null, false, { message: 'No user found' });
+
+        if (user.password !== password) {
+          return done(null, false, { message: 'Wrong password' });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,8 @@
-// /routes/auth.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+  res.send('Login page placeholder');
+});
+
+module.exports = router;
