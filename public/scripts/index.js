b77da0a0fa8c70f28c378d0b62f09860446a9f07
kateemillerhd <kateemillerhd@gmail.com>
2025-07-01 09:12:51 +0000

    Create index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,30 @@
-// index.js
+document.addEvetListener('DOMContentLoaded', () => {
+  const container = document.getElementById('polls-container');
+
+  fetch('/api/polls')
+    .then(res => res.json())
+    .then(polls => {
+      container.innerHTML = '';
+
+      if (polls.length === 0) {
+        container.innerHTML = "<p>No polls available.</p>";
+        return;
+      }
+
+      polls.forEach(poll => {
+        const pollEl = document.createElement('div');
+        pollEl.className = 'poll-card';
+
+        pollEl.innerHTML = `
+        <h2>${poll.question}</h2>
+        <a href="/poll.html?id=${poll._id}">Vote / View Results</a>
+        `;
+
+        container.appendChild(pollEl);
+      });
+    })
+  .catch(err => {
+    console.error('Failed to load polls:', err);
+    container.innerHTML = '<p>Error loading polls.</p>';
+  });
+});
