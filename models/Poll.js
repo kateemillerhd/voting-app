30a5445c674201afffb8b42dac938ef720f40639
kateemillerhd <kateemillerhd@gmail.com>
2025-07-01 22:17:45 +0000

    Create a place to store poll questions, who created poll, and the array with options

--- models/Poll.js
+++ models/Poll.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
 
-const optionSchema = new mongoose.Schema({
-  text: String,
-  votes: { type: Number, default: 0 },
-});
-
 const pollSchema = new mongoose.Schema({
-  question: String,
-  options: [optionSchema],
-  ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  voters: [String],
-  createdAt: { type: Date, default: Date.now },
+  question: {
+    type: String,
+    required: true
+  },
+  options: [
+    {
+      text: {
+        type: String,
+        required: true
+      },
+      votes: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 });
 
 module.exports = mongoose.model("Poll", pollSchema);
