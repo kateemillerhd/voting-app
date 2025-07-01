c3627ae7431d5e0706a59b36f2700b09c569d3d6
kateemillerhd <kateemillerhd@gmail.com>
2025-07-01 09:34:41 +0000

    inputing routes

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const Poll = require('../models/Poll');
+
+router.get('/polls', async (req, res) => {
+  const polls = await Poll.find({}, 'question _id');
+  res.json(polls);
+});
 
 router.get('/test', (req, res) => {
   res.json({ message: "API is working" });
