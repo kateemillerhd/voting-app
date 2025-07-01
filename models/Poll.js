c72b994edea2a2ff1eacab2a2b3a2c04b65b90ad
kateemillerhd <kateemillerhd@gmail.com>
2025-07-01 08:40:55 +0000

    Create Poll model

--- models/Poll.js
+++ models/Poll.js
@@ -1,1 +1,16 @@
-// MongoDB model
+const mongoose = require('mongoose');
+
+const optionSchema = new mongoose.Schema({
+  text: String,
+  votes: { type: Number, default: 0 },
+});
+
+const pollSchema = new mongoose.Schema({
+  question: String,
+  options: [optionSchema],
+  ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  voters: [String],
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Poll", pollSchema);
