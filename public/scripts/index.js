155fb7f15690ae79ab509516c729e3d5b5755778
kateemillerhd <kateemillerhd@gmail.com>
2025-07-01 15:04:04 +0000

    Updating to make user friendly

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.getElementById('polls-container');
+  const nav = document.getElementById("nav");
 
+  let currentUser = null;
+
+  fetch("/auth/me")
+    .then(res => res.json())
+    .then(data => {
+      currentUser = data.user;
+
+      if (currentUser) {
+        nav.innerHTML = `
+        <p>Welcome, <strong>${currentUser}</strong></p>
+        <a href="/create.html">Create Poll</a> |
+        <a href="/my.html">My Polls</a> |
+        <a href="/auth/logout">Logout</a>
+        <hr>
+        `;
+      } else {
+        nav.innerHTML = `
+        <a href="/login.html">Login / Register</a>
+        <hr>
+        `;
+      }
+    });
+  
   fetch('/api/polls')
     .then(res => res.json())
     .then(polls => {
